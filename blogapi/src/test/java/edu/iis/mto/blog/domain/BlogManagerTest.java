eb362ec091d379077b3e915f2302ccdb311fe7f9
Hergoln <juliusz.malka@o2.pl>
2020-05-29 16:57:28 +0200

    zimplementowano testy whenUserWithStatusOtherThanCONFIRMEDPostsLikePostThrowDomainError i userWithStatusCONFIRMEDShouldBeAbleToAddLikePost w klasie BlogManagerTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -3,11 +3,18 @@ package edu.iis.mto.blog.domain;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 
+import edu.iis.mto.blog.api.request.PostRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,12 +27,21 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+    @MockBean
+    private LikePostRepository likePostRepository;
 
     @Autowired
     private BlogDataMapper dataMapper;
@@ -35,6 +51,8 @@ public class BlogManagerTest {
 
     @Captor
     private ArgumentCaptor<User> userParam;
+    @Captor
+    private ArgumentCaptor<LikePost> likeParam;
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -44,4 +62,58 @@ public class BlogManagerTest {
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test(expected = DomainError.class)
+    public void whenUserWithStatusOtherThanCONFIRMEDPostsLikePostThrowDomainError() {
+        User poster = new User();
+        poster.setFirstName("Janusz");
+        poster.setLastName("Janusz");
+        poster.setEmail("janusz@grazyna.pl");
+        poster.setId(1L);
+        poster.setAccountStatus(AccountStatus.NEW);
+        when(userRepository.findById(poster.getId())).thenReturn(Optional.of(poster));
+
+        User liker = new User();
+        poster.setFirstName("Grazyna");
+        poster.setLastName("Grazyna");
+        poster.setEmail("grazyna@janusz.pl");
+        poster.setId(2L);
+        poster.setAccountStatus(AccountStatus.NEW);
+        when(userRepository.findById(liker.getId())).thenReturn(Optional.of(liker));
+
+        BlogPost post = new BlogPost();
+        post.setUser(poster);
+        post.setEntry("test entry");
+        when(blogPostRepository.findById(post.getId())).thenReturn(Optional.of(post));
+
+        blogService.addLikeToPost(liker.getId(), post.getId());
+    }
+
+    @Test
+    public void userWithStatusCONFIRMEDShouldBeAbleToAddLikePost() {
+        User poster = new User();
+        poster.setFirstName("Janusz");
+        poster.setLastName("Janusz");
+        poster.setEmail("janusz@grazyna.pl");
+        poster.setId(1L);
+        when(userRepository.findById(poster.getId())).thenReturn(Optional.of(poster));
+
+        User liker = new User();
+        poster.setFirstName("Grazyna");
+        poster.setLastName("Grazyna");
+        poster.setEmail("grazyna@janusz.pl");
+        poster.setId(2L);
+        poster.setAccountStatus(AccountStatus.CONFIRMED);
+        when(userRepository.findById(liker.getId())).thenReturn(Optional.of(liker));
+
+        BlogPost post = new BlogPost();
+        post.setUser(poster);
+        post.setEntry("test entry");
+        when(blogPostRepository.findById(post.getId())).thenReturn(Optional.of(post));
+
+        assertTrue(blogService.addLikeToPost(liker.getId(), post.getId()));
+        verify(likePostRepository).save(likeParam.capture());
+        LikePost likePost = likeParam.getValue();
+        assertThat(likePost.getUser(), Matchers.equalTo(liker));
+        assertThat(likePost.getPost(), Matchers.equalTo(post));
+    }
 }
