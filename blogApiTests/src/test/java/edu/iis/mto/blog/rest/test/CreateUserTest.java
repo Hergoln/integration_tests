5ee276e5bae9d68c6429404f16682cf662007cda
Hergoln <juliusz.malka@o2.pl>
2020-05-31 10:03:23 +0200

    dodano createUserWithDuplicateEmailReturnsConflictStatus test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -25,4 +25,28 @@ public class CreateUserTest extends FunctionalTests {
                .when()
                .post(USER_API);
     }
+
+    @Test
+    public void createUserWithDuplicateEmailReturnsConflictStatus() {
+        JSONObject jsonObj = new JSONObject()
+                .put("email", "janusz@grazyna.dom")
+                .put("firstName", "Janusz")
+                .put("lastName", "Januszewicz");
+
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .post(USER_API);
+
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CONFLICT)
+                .when()
+                .post(USER_API);
+    }
+
 }
